d69ec816345c97c5f77a17cf5393e0745ebd5e07
hoangmai29 <mnh290905@gmail.com>
2025-07-25 17:39:37 +0700

    Update CicdBtApplication.java

--- CICD_BT/src/main/java/com/example/CICD_BT/CicdBtApplication.java
+++ CICD_BT/src/main/java/com/example/CICD_BT/CicdBtApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CicdBtApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CicdBtApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CicdBtApplication.class, args);
+    }
 }
