227ac24f5e5da9f0a918e002a717623e3065b657
hoangmai29 <mnh290905@gmail.com>
2025-07-27 00:06:38 +0700

    Update Application.java

--- CICD_BT/src/main/java/com/example/CICD_BT/Application.java
+++ CICD_BT/src/main/java/com/example/CICD_BT/Application.java
@@ -1,13 +1,5 @@
 package com.example.CICD_BT;
 
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+    // Không có main method ở đây
 }
-
