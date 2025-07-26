4e9a268125c739a24c7ce7ede4b0e048561a8c25
hoangmai29 <mnh290905@gmail.com>
2025-07-27 00:36:11 +0700

    Update HelloController.java

--- CICD_BT/src/main/java/com/example/CICD_BT/HelloController.java
+++ CICD_BT/src/main/java/com/example/CICD_BT/HelloController.java
@@ -1,13 +1,13 @@
 package com.example.CICD_BT;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
     @GetMapping("/")
     public String hello() {
-        return "Spring Boot chạy trên localhost:9090!";
+        return "Hello, Spring Boot!";
     }
 }
