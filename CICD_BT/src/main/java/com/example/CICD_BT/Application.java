33ee6fa126fd64e0e4c25c1c9b2be3f6a4fd9265
hoangmai29 <mnh290905@gmail.com>
2025-07-26 23:43:47 +0700

    Update Application.java

--- CICD_BT/src/main/java/com/example/CICD_BT/Application.java
+++ CICD_BT/src/main/java/com/example/CICD_BT/Application.java
@@ -1,4 +1,5 @@
-package com.example.cicdbt;
+package com.example.CICD_BT;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
