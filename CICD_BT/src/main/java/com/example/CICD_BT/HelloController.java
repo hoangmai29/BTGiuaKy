e60daaec0d01419b2326548770cda30c7d945b47
hoangmai29 <mnh290905@gmail.com>
2025-07-28 22:08:08 +0700

    Update HelloController.java

--- CICD_BT/src/main/java/com/example/CICD_BT/HelloController.java
+++ CICD_BT/src/main/java/com/example/CICD_BT/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.CICD_BT;
+package com.example.library;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
